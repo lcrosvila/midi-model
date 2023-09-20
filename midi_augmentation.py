06a38a4ce654535ce7405de0174322fe11a70a0d
lcrosvila <lcrosvila@gmail.com>
2023-09-20 22:58:17 +0200

    modified:   midi_augmentation.py 	modified:   scripts.sh

--- midi_augmentation.py
+++ midi_augmentation.py
@@ -1,50 +1,56 @@
 import os
 import random
-import mido
 import argparse
+import pretty_midi
+from copy import deepcopy
 
 # Function to transpose notes in a MIDI file
-def transpose_midi(midi_file, output_file, transpose_semitones):
-    for track in midi_file.tracks:
-        for msg in track:
-            if msg.type == 'note_on':
-                msg.note += transpose_semitones
-    midi_file.save(output_file)
+def transpose_midi(midi, transpose_semitones):
+    for instrument in midi.instruments:
+        for note in instrument.notes:
+            note.pitch += transpose_semitones
 
 # Function to change tempo in a MIDI file
-def change_tempo(midi_file, output_file, tempo_factor):
-    for track in midi_file.tracks:
-        for msg in track:
-            if msg.type == 'set_tempo':
-                msg.tempo = int(msg.tempo * tempo_factor)
-    midi_file.save(output_file)
+def change_tempo(midi, tempo_factor):
+    for instrument in midi.instruments:
+        for note in instrument.notes:
+            note.start *= tempo_factor
+            note.end *= tempo_factor
 
 # Function to vary velocity in a MIDI file
-def vary_velocity(midi_file, output_file, velocity_range):
-    for track in midi_file.tracks:
-        for msg in track:
-            if msg.type == 'note_on':
-                msg.velocity = min(127, max(0, msg.velocity + random.randint(-velocity_range, velocity_range)))
-    midi_file.save(output_file)
+def vary_velocity(midi, velocity_range):
+    for instrument in midi.instruments:
+        for note in instrument.notes:
+            note.velocity = min(127, max(0, note.velocity + random.randint(-velocity_range, velocity_range)))
+
+augmentations = {
+    "transpose": transpose_midi,
+    "tempo": change_tempo,
+    "velocity": vary_velocity
+}
 
 # Function to apply data augmentation techniques to a MIDI file multiple times
 def augment_midi_multiple_times(input_file, output_dir, num_augmentations):
-    midi_file = mido.MidiFile(input_file)
+    midi = pretty_midi.PrettyMIDI(input_file)
     filename = os.path.basename(input_file)
     base_name, ext = os.path.splitext(filename)
 
-    for i in range(num_augmentations):
-        augmentation_suffix = f"augmentation_{i}"
-        output_file = os.path.join(output_dir, f"{base_name}_{augmentation_suffix}{ext}")
-
+    for ii in range(num_augmentations):
         # Apply data augmentation techniques
-        transpose_semitones = random.randint(-5, 5)
+        transpose_semitones = random.randint(-2, 2)
         tempo_factor = random.uniform(0.8, 1.2)
         velocity_range = random.randint(0, 10)
 
-        transpose_midi(midi_file, output_file, transpose_semitones)
-        change_tempo(midi_file, output_file, tempo_factor)
-        vary_velocity(midi_file, output_file, velocity_range)
+        for k, v in augmentations.items():
+            augmented_midi = deepcopy(midi)  # Create a copy of the original MIDI
+            augmentation_suffix = f"{k}_{ii}"
+            output_file = os.path.join(output_dir, f"{base_name}_{augmentation_suffix}{ext}")
+            
+            v(augmented_midi, transpose_semitones) if k == "transpose" else None
+            v(augmented_midi, tempo_factor) if k == "tempo" else None
+            v(augmented_midi, velocity_range) if k == "velocity" else None
+
+            augmented_midi.write(output_file)
 
 # Function to process a directory of MIDI files
 def process_directory(input_dir, output_dir, num_augmentations):
